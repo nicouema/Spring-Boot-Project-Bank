0076f679c2f32f6244a3c60b220ac47ae833b74f
nicouema <nicouemacapdevila@gmail.com>
2022-07-01 23:06:57 -0300

    inserts

---
+++ src/main/resources/db/migration/V1.08__insert_into_document_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO document_type(description, created_at, is_active) VALUES("ID", current_timestamp(), true );
+INSERT INTO document_type(description, created_at, is_active) VALUES("PASSPORT", current_timestamp(), true );
+INSERT INTO document_type(description, created_at, is_active) VALUES("DRIVER LICENSE", current_timestamp(), true );
