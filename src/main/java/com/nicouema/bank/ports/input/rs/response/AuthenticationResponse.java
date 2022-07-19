c18e9971b4dc42f10383db060a8c940e3eb521ce
Nico Uema <nicouemacapdevila@gmail.com>
2022-07-19 01:37:52 -0300

    Update AuthenticationResponse.java

--- src/main/java/com/nicouema/bank/ports/input/rs/response/AuthenticationResponse.java
+++ src/main/java/com/nicouema/bank/ports/input/rs/response/AuthenticationResponse.java
@@ -13,5 +13,5 @@ public class AuthenticationResponse {
     String token;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss Z")
-    Date expirationDAte;
+    Date expirationDate;
 }
