aeececffc943b6aae75a64d281b1785b17a835ed
Nico Uema <nicouemacapdevila@gmail.com>
2022-08-03 18:45:13 -0300

    Update V1.02.4__insert_into_user.sql

--- src/main/resources/db/migration/V1.02.4__insert_into_user.sql
+++ src/main/resources/db/migration/V1.02.4__insert_into_user.sql
@@ -1,3 +1,3 @@
 INSERT INTO user (user_id, email, password, role_id, is_active, created_at)
 VALUES(1, "admin1@bank.com", "$2a$10$KqcUV6vcV5GRkNCYgHxMKuKL69z270Ab4gMG5iLQEmp8VvwvckQVm", 1, true, CURRENT_TIMESTAMP),
-(1, "user1@bank.com", "$2a$10$jGSx69E3UbeUUMMZvb2tF.dNyuGwab8vV1Oqgpx8JupTN.hNegsXm", 2, true, CURRENT_TIMESTAMP)
+(2, "user1@bank.com", "$2a$10$jGSx69E3UbeUUMMZvb2tF.dNyuGwab8vV1Oqgpx8JupTN.hNegsXm", 2, true, CURRENT_TIMESTAMP)
