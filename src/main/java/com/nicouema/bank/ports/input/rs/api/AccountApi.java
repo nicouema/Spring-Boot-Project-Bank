178b6f3e734a13197130d802851ede2fcf65f12c
Nico Uema <nicouemacapdevila@gmail.com>
2022-09-01 17:59:32 -0300

    Update AccountApi.java

--- src/main/java/com/nicouema/bank/ports/input/rs/api/AccountApi.java
+++ src/main/java/com/nicouema/bank/ports/input/rs/api/AccountApi.java
@@ -1,10 +1,20 @@
 package com.nicouema.bank.ports.input.rs.api;
 
+import com.nicouema.bank.common.exception.error.ErrorDetails;
+import com.nicouema.bank.domain.model.AccountId;
 import com.nicouema.bank.domain.model.User;
 import com.nicouema.bank.ports.input.rs.request.CreateAccountRequest;
 import com.nicouema.bank.ports.input.rs.response.AccountListResponse;
 import com.nicouema.bank.ports.input.rs.response.AccountResponse;
 import com.nicouema.bank.ports.input.rs.response.BankStatementListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
@@ -12,18 +22,73 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @Validated
+@SecurityRequirement(name = "bearerAuth")
 public interface AccountApi {
 
+    @Operation(summary = "Create new Account", description = "Create a new account for the current user", responses = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = ErrorDetails.class)),
+                            examples = @ExampleObject(value = "[{\"code\":\"INVALID_FIELD_VALUE\",\"detail\":\"must not be blank\",\"field\":\"name\",\"location\":\"BODY\"}]"))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+    })
     ResponseEntity<Void> createAccount(@Valid CreateAccountRequest createAccountRequest,
                                        User user);
 
+    @Operation(summary = "Get Account by Id", description = "Get an account by id from the current user", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+            @ApiResponse(responseCode = "404", description = "Account not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"RESOURCE_NOT_FOUND\"," +
+                                    "\"detail\":\"The resource with id 99 is not found\"}"))})
+    })
     ResponseEntity<AccountResponse> getAccountById(@Valid Long account_id, @Valid Long branch_id,
                                                    User user);
 
+    @Operation(summary = "Get Bank Statements", description = "Get all user's bank statements order by creation date", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+    })
     ResponseEntity<BankStatementListResponse> getBankStatementDesc(@Valid Long account_id, @Valid Long branch_id,
                                                                    Optional<Integer> page, Optional<Integer> size,
                                                                    User user);
 
+    @Operation(summary = "Get All Bank Accounts", description = "Get all bank accounts", responses = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"BAD_CREDENTIALS\",\"detail\":\"The server cannot return a response due to invalid credentials.\"}"))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = "{\"code\":\"ROLE_INVALID\",\"detail\":\"The user does not have access to the current resource\"}"))}),
+    })
     ResponseEntity<AccountListResponse> getAllAccounts(Optional<Integer> page, Optional<Integer> size);
 
+    ResponseEntity<Void> deleteAccountById(Long accountId, Long branchId);
 }
